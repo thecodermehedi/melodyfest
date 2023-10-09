7903be6e0c5ced8fc7e62f88d0328de9cd5dcf6e
Mehedi Hasan <devbytemehedi@gmail.com>
2023-10-09 22:32:00 +0600

    feat: create About.jsx

---
+++ src/components/About/About.jsx
@@ -0,0 +1,77 @@
+import {Link} from "react-router-dom";
+import {Icon} from "@iconify/react";
+const About = () => {
+  return (
+    <section className="min-h-screen bg-[#181A1B]  p-5 md:p-10 lg:p-20">
+      <h1 className="font-bold uppercase text-xl md:text-2xl  text-blue-500 mb-5">
+        about us
+      </h1>
+      <p className="font-semibold text-xl  md:text-3xl lg:text-5xl text-slate-300 mb-5">
+        We at MelodyFest craft unforgettable musical experiences with passion
+        and precision. Join our extraordinary journey where moments echo long
+        after applause.
+      </p>
+      <div className="block lg:flex justify-between items-center my-10">
+        <p className="font-bold text-white text-opacity-50  md:text-lg  w-80 md:w-full mb-5">
+          Dive into our world of musical wonders. Click to discover the magic
+          behind our unforgettable events.
+        </p>
+        <Link
+          to={"/about"}
+          className="btn rounded-3xl px-5 md:px-8 lg:px-10 bg-transparent border border-blue-500 text-white min-h-0 h-10 lg:h-12 hover:bg-blue-500 hover:text-white hover:border-blue-500 normal-case font-bold text-xl"
+        >
+          About Us
+        </Link>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 place-items-center lg:place-items-start gap-10">
+        <div className="w-full h-fit lg:h-[21rem] bg-black bg-opacity-50 rounded-2xl p-8">
+          <div className="p-5 mb-5 rounded-2xl bg-[#181A1B] w-fit">
+            <Icon
+              icon="solar:music-note-outline"
+              className="text-blue-500 text-3xl md:text-5xl lg:text-7xl"
+            />
+          </div>
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-white">
+            Harmonious
+          </h1>
+          <p className=" text-lg md:text-xl lg:text-2xl font-semibold text-white text-opacity-50">
+            Experience harmonious celebrations and musical wonders at
+            MelodyFest.
+          </p>
+        </div>
+        <div className="w-full h-fit lg:h-[21rem] bg-black bg-opacity-50 rounded-2xl p-8">
+          <div className="p-5 mb-5 rounded-2xl bg-[#181A1B] w-fit">
+            <Icon
+              icon="iconoir:lens"
+              className="text-blue-500 text-3xl md:text-5xl lg:text-7xl"
+            />
+          </div>
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-white">
+            Captivating
+          </h1>
+          <p className=" text-lg md:text-xl lg:text-2xl font-semibold text-white text-opacity-50">
+            Explore captivating concerts and immersive festivals that linger in
+            your heart.
+          </p>
+        </div>
+        <div className="w-full h-fit lg:h-[21rem] bg-black bg-opacity-50 rounded-2xl p-8">
+          <div className="p-5 mb-5 rounded-2xl bg-[#181A1B] w-fit">
+            <Icon
+              icon="fluent-emoji-high-contrast:left-speech-bubble"
+              className="text-blue-500 text-3xl md:text-5xl lg:text-7xl"
+            />
+          </div>
+          <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-white">
+            Memorable
+          </h1>
+          <p className=" text-lg md:text-xl lg:text-2xl font-semibold text-white text-opacity-50">
+            Create memorable moments with us, where melodies echo long after the
+            applause.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
